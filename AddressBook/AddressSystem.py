fe13645cb7073903d20b5fea91a681927ac7045e
唐维冬 <wade@wadedeMac.local>
2017-11-20 11:08:43 +0800

    提交新代码

---
+++ AddressBook/AddressSystem.py
@@ -0,0 +1,49 @@
+#连接数据库
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import pymysql.cursors
+import os
+import time
+import print_message
+import EditorAddress
+
+
+while True:
+    print_message.print_menu()
+    runkey = int(input("\n请输入需要的操作编号："))
+    if runkey==1:
+        EditorAddress.Query_address()
+        #查询联系人
+    elif runkey==2:
+        EditorAddress.Update_Address()
+        #更新联系人
+    elif runkey==3:
+        EditorAddress.Insert_Address()
+        #插入联系人
+
+    elif runkey==4:
+        EditorAddress.Del_address()
+        #删除联系人
+    elif runkey==5:
+        #导出联系人
+        return_key = EditorAddress.Export_All()
+        if return_key == True:
+            print('\n导出成功！！！！')
+        else:
+            print('导出失败')
+    elif runkey==6:
+        break
+    elif runkey==None:
+        break
+    else:
+        print("请输入正确的选项！！！\n \n")
+
+
+
+
+
+
+
+
+
+
---
+++ AddressBook/print_message.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+def print_menu():
+    #os.system('Clear')
+    print('\n1、查询联系人')
+    print('2、更新联系人')
+    print('3、插入联系人')
+    print('4、删除联系人')
+    print('5、导出联系人')
+    print('6、退出程序')
+
+def print_cxmenu():
+    print('1、通过编号查询')
+    print('2、通过名称查询')
+    print('3、通过性别查询')
+    print('4、通过年龄查询')
+    print('5、通过手机查询')
+    print('6、查询所有联系人')
+    print('7、清空屏幕显示')
+    print('8、返回主菜单')
+
+
+def Qc_Clear():
+    os.system('Clear')
